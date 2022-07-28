2f668e10bf171fe0a916acc6e95f9cf565f14234
Andre Bonnano <andrebonnano@hotmail.com>
2022-07-27 22:58:50 -0300

    Edição de tasks - finalização

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,47 @@ import TaskList from './components/TaskList';
 import { ITask } from './interfaces/ITask';
 
 import styles from './css/App.module.css'
+import Modal from './components/Modal';
 
 function App() {
 
   const [taskList, setTaskList] = useState<ITask[]>([])
+  const [taskToUpdate, setTaskToUpdate] = useState<ITask | null>(null)
 
-  const deleteTask = (id: number) =>{ 
+  const deleteTask = (id: number) => {
     setTaskList(
-      taskList.filter(task=>{
+      taskList.filter(task => {
         return task.id !== id
       })
     )
   }
 
+  const hideOrShowModal = (display: boolean) => {
+    const modal = document.querySelector('#modal')
+    if (display) {
+      modal!.classList.remove('hide')
+    } else {
+      modal!.classList.add('hide')
+    }
+  }
+
+  const editTask = (task: ITask): void => {
+    hideOrShowModal(true)
+    setTaskToUpdate(task)
+  }
+
+  const updateTask = (id: number, title: string, difficulty: number) => {
+    const updatedTask: ITask = { id, title, difficulty }
+    const updatedItems = taskList.map((task) => {
+      return task.id === updatedTask.id ? updatedTask : task
+    })
+    setTaskList(updatedItems)
+    hideOrShowModal(false)
+  }
+
   return (
     <div>
+      <Modal children={<TaskForm btnText='Editar tarefa' taskList={taskList} task={taskToUpdate} handleUpdate={updateTask} />} ></Modal>
       <Header></Header>
       <main className={styles.main}>
         <div>
@@ -28,7 +54,7 @@ function App() {
         </div>
         <div>
           <h2>Suas Tarefas</h2>
-          <TaskList taskList={taskList} handelDelete={deleteTask}></TaskList>
+          <TaskList taskList={taskList} handelDelete={deleteTask} handleEdit={editTask}></TaskList>
         </div>
       </main>
       <Footer></Footer>
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -4,29 +4,30 @@ import styles from '../css/TaskList.module.css'
 
 type Props = {
   taskList: ITask[]
-  handelDelete(id:number):void
+  handelDelete(id: number): void
+  handleEdit(task: ITask): void
 }
 
-const TaskList = ({ taskList, handelDelete }: Props) => {
+const TaskList = ({ taskList, handelDelete, handleEdit }: Props) => {
   return (
-      <>
-        {taskList.length > 0 ? (
-          taskList.map((task) =>(
-            <div key={task.id} className={styles.task}>
-              <div className={styles.details}>
-                <h4>{task.title}</h4>
-                <p>Dificuldade: {task.difficulty}</p>
-              </div>
-              <div className={styles.actions}>
-                <i className="bi bi-pencil"></i>
-                <i className="bi bi-trash" onClick={() => {handelDelete(task.id)}}></i>
-              </div>
+    <>
+      {taskList.length > 0 ? (
+        taskList.map((task) => (
+          <div key={task.id} className={styles.task}>
+            <div className={styles.details}>
+              <h4>{task.title}</h4>
+              <p>Dificuldade: {task.difficulty}</p>
             </div>
-          ))
-        ) : (
-          <p>Não há tarefas cadastradas!</p>
-        )}
-      </>
+            <div className={styles.actions}>
+              <i className="bi bi-pencil" onClick={() => { handleEdit(task) }}></i>
+              <i className="bi bi-trash" onClick={() => { handelDelete(task.id) }}></i>
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>Não há tarefas cadastradas!</p>
+      )}
+    </>
   )
 }
 
